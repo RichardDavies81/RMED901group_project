f0ac99f8858b284d7e7eeb411e9f22f6f194b540
Richard Davies <richard.davies@uib.no>
2024-09-09 13:20:29 +0200

    Folder setup scripts

---
+++ scripts/setup.R
@@ -0,0 +1,15 @@
+# setup ----
+
+library(tidyverse) # loading package for cleaning of data
+# install.packages("tidyverse")
+
+library(here) # for targeting directory
+here()
+
+dir.create("data")
+dir.create("figures")
+dir.create("processed_data")
+dir.create("results")
+dir.create("scripts")
+
+
