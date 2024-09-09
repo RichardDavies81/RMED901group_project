b534efea7330cd4ae05a9f71d26e33ca72dadece
Richard Davies <richard.davies@uib.no>
2024-09-09 13:54:15 +0200

    Spliting column 1 and 2

--- scripts/setup.R
+++ scripts/setup.R
@@ -12,4 +12,19 @@ dir.create("processed_data")
 dir.create("results")
 dir.create("scripts")
 
+## explore_dataset.txt ----
 
+my_data <- read_delim("data/exam_dataset.txt", col_names = TRUE)
+
+my_data <- my_data %>% 
+  separate(col = 2, 
+           into = c("gender", "age"), 
+           sep = "-")
+
+my_data <- my_data %>% 
+  separate(col = 1, 
+           into = c("ID", "first_name", "last_name"), 
+           sep = " ")
+
+
+# separate gender and age
