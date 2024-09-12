ca434804bc3ae57406f0592a310ccf69d14783b8
Richard Davies <richard.davies@uib.no>
2024-09-12 16:05:36 +0200

    Shortened "drive-through" script with correction to tidy data tabel

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -14,19 +14,11 @@ test <- complete_data %>%
 # Are there more positive tests in the drive-through?
 
 
-dt_results <- complete_data %>%
-  select(dti_yes_no, result) %>%
-  mutate(dti_yes_no = factor(dti_yes_no))
-
-unique(dt_results$result)
-
-table <- dt_results %>%
+table <- complete_data %>%
   group_by(dti_yes_no, result) %>%
   count() 
 
-chisq.test(x = dt_results$dti_yes_no, y = dt_results$result)
-
-
+chisq.test(x = complete_data$dti_yes_no, y = complete_data$result)
 
 
 # Is the age of the individual associated with whether the test result is negative or positive?
