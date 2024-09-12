c8ac232bf2dc970c3268b70f222e82a42154b9a4
pakwanja-desiree <pakwanja.twea@gmail.com>
2024-09-12 12:58:40 +0200

    resolve conflict

--- scripts/viz.R
+++ scripts/viz.R
@@ -1,3 +1,77 @@
+# Script for visualization of data from tidy data generated in setup.R script
+
+
+# note- each person chooses min.one question
+
+# Are there any correlated measurements?
+
+
+# Does the time spent waiting for the test result improve during these 100 days that the dataset includes?
+
+
+# Were there more females than males that took the test at a drive through?
+dt_by_gender <- complete_data %>%
+  group_by(gender) %>%
+  count(drive_thru_ind)
+
+dt_by_gender
+
+ggplot(data = dt_by_gender, aes(x = drive_thru_ind, y = n, fill = gender)) +
+  geom_bar(stat = "identity", color="black", position=position_dodge())+
+  theme_minimal() + 
+  scale_fill_manual(values=c('#999999','#E69F00')) +
+# Use brewer color palettes
+  scale_fill_brewer(palette="Blues")
+
+# Does the distribution of the `ct_result` differ with sex group?
+ct_result_dist <- complete_data %>%
+  group_by(gender) %>%
+  count(ct_result)
+
+ggplot(ct_result_dist, 
+       aes(
+         x = 1:nrow(ct_result_dist),
+         y = ct_result,
+         group = gender,
+         color = gender)
+       ) + 
+  geom_point() +
+  facet_wrap(vars(gender))
+  
+
+# Does the distribution of the `ct_result` differ with `payor_group`?
+# Subset the data
+ct_result_dist_pg <- complete_data %>%
+  group_by(payor_group) %>%
+  count(ct_result)
+
+ggplot(
+  ct_result_dist_pg,
+  aes(
+    x = 1:nrow(ct_result_dist_pg),
+    y = ct_result,
+    group = payor_group,
+    color = payor_group
+    )
+  ) +
+  geom_point() +
+  facet_wrap(vars(payor_group))
+
+
+# Were there more tests in any of the sex groups?
+tests_by_gender <- complete_data %>%
+  group_by(gender) %>%
+  count(result)
+
+tests_by_gender
+
+ggplot(data = tests_by_gender, aes(x = result, y = n, fill = gender)) +
+  geom_bar(stat = "identity", color="black", position=position_dodge())+
+  theme_minimal() +
+  scale_fill_manual(values=c('#999999','#E69F00')) +
+# Use brewer color palettes
+  scale_fill_brewer(palette="Blues")
+
 
 # Script for visualization of data from tidy data generated in setup.R script
 
