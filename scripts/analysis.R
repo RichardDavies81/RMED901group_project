3c629be2bdcbda6cb94c24ff5f088c694928470d
pakwanja-desiree <pakwanja.twea@gmail.com>
2024-09-13 08:33:37 +0200

    update analysis script

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -5,27 +5,37 @@
 # Do your chances of getting a positive test increase with the number of tests taken?
 
 
-
 # Are there more positive tests in the drive-through?
-
-
-dt_results <- complete_data %>%
-  select(dti_yes_no, result) %>%
-  mutate(dti_yes_no = factor(dti_yes_no))
-
-unique(dt_results$result)
-
-table <- dt_results %>%
-  group_by(dti_yes_no, result) %>%
-  count() 
-
-chisq.test(x = dt_results$dti_yes_no, y = dt_results$result)
-
 
 
 
 # Is the age of the individual associated with whether the test result is negative or positive?
+# first check associations via plots
+#create a categorical age variable
 
+complete_data$age_cat <- cut(complete_data$age, 
+                             breaks = c(0, 9, 19, 29, 39, 49, 59, 69, 79, 89, 99, Inf),
+                             labels = c("0-9", "10-19", "20-29", "30-39", 
+                                        "40-49", "50-59", "60-69", "70-79", 
+                                        "80-89", "90-99", "100+"))
+
+arrange(complete_data, by = ID)
+complete_data %>% select(ID, age, age_cat, gender, everything())
+
+ggplot(complete_data, 
+       aes(
+         x = result,
+         y = age_cat,
+         group = gender,
+         color = gender)
+) + 
+  geom_point() +
+  facet_wrap(vars(gender))
+
+# ANOVA
+complete_data_aov <- aov(complete_data$age~complete_data$result)
+complete_data_aov
+summary(complete_data_aov)
 
 
 # Is there a difference in the distributions of `ct_results` between different outcome groups (`result`)?
